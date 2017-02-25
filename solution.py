ec0ccd6c8462d7b5bb5bcbda55865325e2227724
Charlie Summers <csummers@42.us.org>
2017-02-24 20:21:45 -0800

    Uncomment pygame functionality for correctors.

--- solution.py
+++ solution.py
@@ -163,11 +163,11 @@ if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(solve(diag_sudoku_grid))
 
-    #try:
-    #    from visualize import visualize_assignments
-    #    visualize_assignments(assignments)
+    try:
+        from visualize import visualize_assignments
+        visualize_assignments(assignments)
 
-    #except SystemExit:
-    #    pass
-    #except:
-    #    print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
+    except SystemExit:
+        pass
+    except:
+        print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
